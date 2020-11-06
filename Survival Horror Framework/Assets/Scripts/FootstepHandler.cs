beb9d3e5c06fdaa7d8ecffff060d3264f9f5a201
LEAKYFINGERS <weepingdigits@gmail.com>
2020-11-06 16:44:19 +1100

    Started working on footstep system

---
+++ Survival Horror Framework/Assets/Scripts/FootstepHandler.cs
@@ -0,0 +1,61 @@
+﻿//////////////////////////////////////////////////
+// Author/s:            LEAKYFINGERS   
+// Date created:        06.11.20
+// Date last edited:    06.11.20
+//////////////////////////////////////////////////
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SurvivalHorrorFramework
+{
+    public class FootstepHandler : MonoBehaviour
+    {
+        public AudioSource LeftFoot;
+        public AudioSource RightFoot;
+        public AudioClip TestFootstepWalkSound; // DEBUG
+        public AudioClip TestFootstepRunSound; // DEBUG
+
+        public void PlayLeftFootstepWalk()
+        {
+            if (!LeftFoot.isPlaying)
+            {
+                LeftFoot.clip = TestFootstepWalkSound;
+                LeftFoot.Play();
+            }
+        }
+
+        public void PlayRightFootstepWalk()
+        {
+            if (!RightFoot.isPlaying)
+            {
+                RightFoot.clip = TestFootstepWalkSound;
+                RightFoot.Play();
+            }
+        }
+
+        public void PlayLeftFootstepRun()
+        {
+            if (!LeftFoot.isPlaying)
+            {
+                LeftFoot.clip = TestFootstepRunSound;
+                LeftFoot.Play();
+            }
+        }
+
+        public void PlayRightFootstepRun()
+        {
+            if (!RightFoot.isPlaying)
+            {
+                RightFoot.clip = TestFootstepRunSound;
+                RightFoot.Play();
+            }
+        }
+
+
+        //private Material GetFloorMaterial()
+        //{
+
+        //}
+    }
+}
