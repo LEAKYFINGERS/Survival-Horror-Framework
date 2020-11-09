57429834644420590670cd73627f81ce03904eef
LEAKYFINGERS <weepingdigits@gmail.com>
2020-11-09 18:11:03 +1100

    More work on footstep system

--- Survival Horror Framework/Assets/Scripts/FootstepHandler.cs
+++ Survival Horror Framework/Assets/Scripts/FootstepHandler.cs
@@ -1,7 +1,7 @@
 ﻿//////////////////////////////////////////////////
 // Author/s:            LEAKYFINGERS   
 // Date created:        06.11.20
-// Date last edited:    06.11.20
+// Date last edited:    09.11.20
 //////////////////////////////////////////////////
 using System.Collections;
 using System.Collections.Generic;
@@ -13,14 +13,15 @@ namespace SurvivalHorrorFramework
     {
         public AudioSource LeftFoot;
         public AudioSource RightFoot;
-        public AudioClip TestFootstepWalkSound; // DEBUG
-        public AudioClip TestFootstepRunSound; // DEBUG
+        public List<FootstepFloorMaterial> FootstepFloorMaterials;
+        // TODO - add default footstep walk and run sounds
+        // TODO - add support for arrays of walk + run sounds and randomise which is picked
 
         public void PlayLeftFootstepWalk()
         {
             if (!LeftFoot.isPlaying)
-            {
-                LeftFoot.clip = TestFootstepWalkSound;
+            {                
+                LeftFoot.clip = GetFootstepFloorMaterialBelowPlayer().FootstepWalkSound;
                 LeftFoot.Play();
             }
         }
@@ -29,7 +30,7 @@ namespace SurvivalHorrorFramework
         {
             if (!RightFoot.isPlaying)
             {
-                RightFoot.clip = TestFootstepWalkSound;
+                RightFoot.clip = GetFootstepFloorMaterialBelowPlayer().FootstepWalkSound;
                 RightFoot.Play();
             }
         }
@@ -38,7 +39,7 @@ namespace SurvivalHorrorFramework
         {
             if (!LeftFoot.isPlaying)
             {
-                LeftFoot.clip = TestFootstepRunSound;
+                LeftFoot.clip = GetFootstepFloorMaterialBelowPlayer().FootstepRunSound;
                 LeftFoot.Play();
             }
         }
@@ -46,16 +47,34 @@ namespace SurvivalHorrorFramework
         public void PlayRightFootstepRun()
         {
             if (!RightFoot.isPlaying)
-            {
-                RightFoot.clip = TestFootstepRunSound;
+            {               
+                RightFoot.clip = GetFootstepFloorMaterialBelowPlayer().FootstepRunSound; 
                 RightFoot.Play();
             }
         }
 
 
-        //private Material GetFloorMaterial()
-        //{
+        private FootstepFloorMaterial GetFootstepFloorMaterialBelowPlayer()
+        {
+            RaycastHit raycastHit;
+            Ray ray = new Ray(transform.position, Vector3.down); // TODO - add vector offset for ray origin from transform position.
+            if (Physics.Raycast(ray, out raycastHit))
+            {
+                Material floorMaterial = raycastHit.collider.GetComponent<Renderer>().material;
+                foreach(FootstepFloorMaterial footstepFloorMaterial in FootstepFloorMaterials)
+                {
+                    if(floorMaterial == footstepFloorMaterial.FloorMaterial)
+                    {
+                        return footstepFloorMaterial;
+                    }
+                }
 
-        //}
+                throw new System.Exception("The FootstepFloorMaterials list of the FootstepHandler script does not contain any FootstepFloorMaterial objects with the same material as assigned to the Renderer of the floor on which the attached character is standing.");
+            }
+            else
+            {
+                throw new System.Exception("There is no collider currently below the object which the FootstepHandler is assigned to.");
+            }
+        }
     }
 }
